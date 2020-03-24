5010bd4522b6531d4717605ee6ab52818ae632a9
Mateo Canabal <realmadridfan133@protonmail.com>
2020-03-24 14:13:53 -0500

    Added JSON fuctionality, a config file, and modified the timer

--- JetsonNodeDataAgent/NodeClient.cs
+++ JetsonNodeDataAgent/NodeClient.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Net;
+using System.Net.Sockets;
 using System.Diagnostics;
 using System.Threading;
+using RestSharp;
+using System.Collections.Generic;
 
 namespace JetsonNodeDataAgent
 {
@@ -17,45 +20,90 @@ namespace JetsonNodeDataAgent
         private uint used_mem;          //MB
         private uint total_mem;         //MB
         private int frequency;          //Hz
+        private String JetsonServiceIP; // Standard IPv4 address
+        private uint NodeID, ClusterID;
+        private RestClient ServiceClient;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="NodeClient"/> class.
         /// </summary>
         /// <remarks>
-        /// Initializes frequency to a default of 1 Hz and obtains the rest of
-        /// the values automatically.
+        /// Data is obtained from NodeClientConfig.txt or automatically.
         /// </remarks>
         public NodeClient()
         {
-            frequency = 1;                  //1 Hz is default frequency
+            string ConfigFile = System.IO.File.ReadAllText(@"NodeClientConfig.txt");
+            string[] SplitConfigFile = ConfigFile.Split(new Char[] { '\n' });
+
+            JetsonServiceIP = SplitConfigFile[0].Replace("JetsonServiceIP=", "");
+            NodeID = UInt32.Parse(SplitConfigFile[1].Replace("NodeID=", ""));
+            ClusterID = UInt32.Parse(SplitConfigFile[2].Replace("ClusterID=", ""));
+            frequency = Int32.Parse(SplitConfigFile[3].Replace("Frequency=", ""));
             host_name = Dns.GetHostName();
             num_cores = DetermineNumCores();
             total_mem = DetermineMemTotal();
             cpu_usage = new float[num_cores];
+            ServiceClient = new RestClient("https://" + JetsonServiceIP);
         }
 
-        /// <summary>
-        /// ChangeFrequency accepts an int value for frequency as an argument and sets Program's value to it.
-        /// </summary>
-        /// <param name="freq"></param>
-        public void ChangeFrequency(int freq) => frequency = freq;  //replace eventually with thread that listens for JSON from master. Also make threadsafe for that.
+        public static string GetLocalIPAddress() // source: https://stackoverflow.com/questions/6803073/get-local-ip-address
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
 
         /// <summary>
         /// SendData currently prints out node values for debugging purposes, but in its final
         /// form, it will transmit JSON files containing the data to the master node running
         /// JetsonService.
         /// </summary>
-        private void SendData()
+        private void SendData(Object stateInfo)
         {
-            //replace eventually with generating JSON with data and sending it via HTTPS to master node
-            Console.WriteLine("Host name = " + host_name);
-            Console.WriteLine("Number of cores = " + num_cores.ToString());
-            Console.WriteLine("Total memory = " + total_mem.ToString());
-            Console.WriteLine("Used memory = " + used_mem.ToString());
-            for (int i = 0; i < num_cores; i++)
+            var node = new Node()
             {
-                Console.WriteLine("Core " + i.ToString() + " Usage = " + cpu_usage[i].ToString() + "%.");
+                Id = NodeID,
+                IPAddress = GetLocalIPAddress()
+            };
+
+            var cores = new List<CpuCore>();
+            for (int i = 0; i < cpu_usage.Length; i++)
+            {
+                cores.Add(new CpuCore() { CoreNumber = i, UtilizationPercentage = cpu_usage[i] });
             }
+
+            var nodeUtilization = new NodeUtilization()
+            {
+                GlobalNodeId = ClusterID * 100 + NodeID,
+                MemoryAvailable = total_mem - used_mem,
+                MemoryUsed = used_mem,
+                TimeStamp = DateTime.Now,
+                Cores = cores,
+            };
+
+            var request = new RestRequest();
+
+            request.Method = Method.POST;
+            request.AddHeader("Accept", "application/json");
+            request.Parameters.Clear();
+            request.AddJsonBody(node);
+
+            ServiceClient.Execute(request);
+
+            request = new RestRequest();
+
+            request.Method = Method.POST;
+            request.AddHeader("Accept", "application/json");
+            request.Parameters.Clear();
+            request.AddJsonBody(nodeUtilization);
+
+            ServiceClient.Execute(request);
         }
 
         /// <summary>
@@ -63,7 +111,8 @@ namespace JetsonNodeDataAgent
         /// </summary>
         private void UpdateMemory()
         {
-            string cat_proc_meminfo_output = Bash("cat /proc/meminfo");
+            //string cat_proc_meminfo_output = Bash("cat /proc/meminfo");
+            string cat_proc_meminfo_output = System.IO.File.ReadAllText(@"/proc/meminfo");
             cat_proc_meminfo_output = cat_proc_meminfo_output.Replace(" ", "");     //remove spaces
             cat_proc_meminfo_output = cat_proc_meminfo_output.Replace("kB", "");    //remove kB
 
@@ -87,7 +136,8 @@ namespace JetsonNodeDataAgent
         {
             // Find phase 1 then phase 2 in order to find change.
 
-            string cat_proc_stat_output_phase1 = Bash("cat /proc/stat");
+            //string cat_proc_stat_output_phase1 = Bash("cat /proc/stat");
+            string cat_proc_stat_output_phase1 = System.IO.File.ReadAllText(@"/proc/stat");
             uint[] active_cpu_phase1 = new uint[num_cores];
             uint[] total_cpu_phase1 = new uint[num_cores];
             
@@ -115,7 +165,8 @@ namespace JetsonNodeDataAgent
 
             // Find phase 2.
 
-            string cat_proc_stat_output_phase2 = Bash("cat /proc/stat");
+            //string cat_proc_stat_output_phase2 = Bash("cat /proc/stat");
+            string cat_proc_stat_output_phase2 = System.IO.File.ReadAllText(@"/proc/stat");
             uint[] active_cpu_phase2 = new uint[num_cores];
             uint[] total_cpu_phase2 = new uint[num_cores];
 
@@ -147,6 +198,12 @@ namespace JetsonNodeDataAgent
             }
         }
 
+        private void Update(Object stateInfo)
+        {
+            UpdateMemory();
+            UpdateCPUUsage();
+        }
+
         /// <summary>
         /// DetermineNumCores finds the number of cores on the system.
         /// </summary>
@@ -160,7 +217,8 @@ namespace JetsonNodeDataAgent
         /// </summary>
         private uint DetermineMemTotal()
         {
-            string cat_proc_meminfo_output = Bash("cat /proc/meminfo");
+            //string cat_proc_meminfo_output = Bash("cat /proc/meminfo");
+            string cat_proc_meminfo_output = System.IO.File.ReadAllText(@"/proc/meminfo");
             cat_proc_meminfo_output = cat_proc_meminfo_output.Replace(" ", "");     //remove spaces
             cat_proc_meminfo_output = cat_proc_meminfo_output.Replace("kB", "");    //remove kB
 
@@ -213,12 +271,15 @@ namespace JetsonNodeDataAgent
         static void Main(string[] args)
         {
             NodeClient myProgram = new NodeClient();
-            while(true)
-            {
-                Thread.Sleep(500 / myProgram.frequency);    // Wait half period. Other half in CPU usage function
-                myProgram.UpdateMemory();
-                myProgram.SendData();
-            }
+
+            var autoEventUpdate = new AutoResetEvent(false);
+            var autoEventSend = new AutoResetEvent(false);
+
+            var UpdateTimer = new Timer(myProgram.Update, autoEventUpdate, 0, 1000 / myProgram.frequency);
+            var SendTimer = new Timer(myProgram.SendData, autoEventUpdate, 1000 / myProgram.frequency, 1000 / myProgram.frequency);
+
+            autoEventUpdate.WaitOne();  // neither AutoResetEvent objects will ever be set, so the threads will run indefinitely.
+            autoEventSend.WaitOne();
         }
     }
 }
